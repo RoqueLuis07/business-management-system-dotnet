88f95693d9242d5e7e0d667c9fdd57d81abfc3e7
RoqueLuis07 <roqueesteche6543@gmail.com>
2026-02-04 21:50:59 -0300

    Add use case to start work order diagnosis

---
+++ src/Application/BusinessManagementSystem.Application/WorkOrders/StartWorkOrderDiagnosis.cs
@@ -0,0 +1,20 @@
+﻿using BusinessManagementSystem.Application.Abstractions;
+
+namespace BusinessManagementSystem.Application.WorkOrders
+{
+    public static class StartWorkOrderDiagnosis
+    {
+        public record Command(Guid WorkOrderId);
+
+        public static async Task HandleAsync(IWorkOrderRepository repo, Command cmd, CancellationToken ct)
+        {
+            var wo = await repo.GetByIdAsync(cmd.WorkOrderId, ct);
+            if (wo is null)
+                throw new InvalidOperationException("No se encontró la OT.");
+
+            wo.StartDiagnosis();
+
+            await repo.UpdateAsync(wo, ct);
+        }
+    }
+}
