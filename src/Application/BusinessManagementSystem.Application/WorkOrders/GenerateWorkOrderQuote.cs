4aeed4d489654bb547f6c70aa9463387001271f9
RoqueLuis07 <roqueesteche6543@gmail.com>
2026-02-04 21:59:26 -0300

    Add use case to generate work order quote

---
+++ src/Application/BusinessManagementSystem.Application/WorkOrders/GenerateWorkOrderQuote.cs
@@ -0,0 +1,30 @@
+﻿using BusinessManagementSystem.Application.Abstractions;
+
+namespace BusinessManagementSystem.Application.WorkOrders
+{
+    public static class GenerateWorkOrderQuote
+    {
+        public record Command(
+            Guid WorkOrderId,
+            decimal LaborCost,
+            string? Notes,
+            Guid CreatedByUserId);
+
+        public static async Task HandleAsync(
+            IWorkOrderRepository repo,
+            Command cmd,
+            CancellationToken ct)
+        {
+            var wo = await repo.GetByIdAsync(cmd.WorkOrderId, ct);
+            if (wo is null)
+                throw new InvalidOperationException("No se encontró la OT.");
+
+            wo.CreateOrUpdateQuote(
+                cmd.LaborCost,
+                cmd.Notes,
+                cmd.CreatedByUserId);
+
+            await repo.UpdateAsync(wo, ct);
+        }
+    }
+}
