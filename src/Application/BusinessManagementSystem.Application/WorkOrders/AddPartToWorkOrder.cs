e447e86a0865fdcff33661ae04ee8dd568efdcae
RoqueLuis07 <roqueesteche6543@gmail.com>
2026-02-04 21:56:17 -0300

    Add use case to add parts to work orders

---
+++ src/Application/BusinessManagementSystem.Application/WorkOrders/AddPartToWorkOrder.cs
@@ -0,0 +1,20 @@
+﻿using BusinessManagementSystem.Application.Abstractions;
+
+namespace BusinessManagementSystem.Application.WorkOrders
+{
+    public static class AddPartToWorkOrder
+    {
+        public record Command(Guid WorkOrderId, string PartName, int Quantity);
+
+        public static async Task HandleAsync(IWorkOrderRepository repo, Command cmd, CancellationToken ct)
+        {
+            var wo = await repo.GetByIdAsync(cmd.WorkOrderId, ct);
+            if (wo is null)
+                throw new InvalidOperationException("No se encontró la OT.");
+
+            wo.AddPart(cmd.PartName, cmd.Quantity);
+
+            await repo.UpdateAsync(wo, ct);
+        }
+    }
+}
