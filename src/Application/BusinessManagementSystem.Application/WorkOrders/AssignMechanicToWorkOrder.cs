0898bdadd84d7807f5925a50f2802972fac41404
RoqueLuis07 <roqueesteche6543@gmail.com>
2026-02-04 21:49:30 -0300

    Add use case to assign mechanic to work orders

---
+++ src/Application/BusinessManagementSystem.Application/WorkOrders/AssignMechanicToWorkOrder.cs
@@ -0,0 +1,20 @@
+﻿using BusinessManagementSystem.Application.Abstractions;
+
+namespace BusinessManagementSystem.Application.WorkOrders
+{
+    public static class AssignMechanicToWorkOrder
+    {
+        public record Command(Guid WorkOrderId, Guid MechanicUserId);
+
+        public static async Task HandleAsync(IWorkOrderRepository repo, Command cmd, CancellationToken ct)
+        {
+            var wo = await repo.GetByIdAsync(cmd.WorkOrderId, ct);
+            if (wo is null)
+                throw new InvalidOperationException("No se encontró la OT.");
+
+            wo.AssignMechanic(cmd.MechanicUserId);
+
+            await repo.UpdateAsync(wo, ct);
+        }
+    }
+}
